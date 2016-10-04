4c5c589d4aebee49d1e3c49a8d5ec9b4f3d20aa8
Htoo Maung Thait <robinhtoo@gmail.com>
2016-10-04 06:37:00 +0630

    Collapsing Toolbar Layout was changed to display MM font

---
+++ app/src/main/java/com/padc/interactive_training/components/MMCollapsingToolbarLayout.java
@@ -0,0 +1,28 @@
+package com.padc.interactive_training.components;
+
+import android.content.Context;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.util.AttributeSet;
+
+import com.padc.interactive_training.utils.MMFontUtils;
+
+/**
+ * Created by htoomt on 10/4/2016.
+ */
+
+public class MMCollapsingToolbarLayout extends CollapsingToolbarLayout {
+    public MMCollapsingToolbarLayout(Context context) {
+        super(context);
+        MMFontUtils.applyMMFontToCollapsingToolbar(this);
+    }
+
+    public MMCollapsingToolbarLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        MMFontUtils.applyMMFontToCollapsingToolbar(this);
+    }
+
+    public MMCollapsingToolbarLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        MMFontUtils.applyMMFontToCollapsingToolbar(this);
+    }
+}
